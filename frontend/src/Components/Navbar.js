fba0aec3b93186dfdf4f115f13c950afb3ea4d86
RyanGuzelian <ryanguzelian@gmail.com>
2023-11-14 22:36:48 -0500

    Changed navbar

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,84 +1,121 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
-import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from "@mui/icons-material/Menu";
 import Drawer from "@mui/material/Drawer";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
+import { CssBaseline, useMediaQuery, useTheme } from "@mui/material";
 
 const Navbar = () => {
-  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [mobileOpen, setMobileOpen] = useState(false);
   const navigate = useNavigate();
+  const theme = useTheme();
+  const isMobileSize = useMediaQuery(theme.breakpoints.down("sm"));
 
   const handleDrawerToggle = () => {
-    setDrawerOpen(!drawerOpen);
+    setMobileOpen((prevState) => !prevState);
   };
 
   const handleNavigation = (path) => {
     navigate(path);
-    setDrawerOpen(false); // Close drawer after navigation
+    setMobileOpen(false);
   };
 
   const drawer = (
-      <Box
-          sx={{ width: 250 }}
-          role="presentation"
-          onClick={handleDrawerToggle}
-          onKeyDown={handleDrawerToggle}
-      >
-        <List>
-          {['Track', 'Ship', 'Contact Us'].map((text) => (
-              <ListItem
-                  button
-                  key={text}
-                  onClick={() => handleNavigation(text === 'Track' ? '/track' : text === 'Ship' ? '/ship' : '/contact')}
-              >
-                <ListItemText primary={text} />
-              </ListItem>
-          ))}
-        </List>
-      </Box>
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={handleDrawerToggle}
+      onKeyDown={handleDrawerToggle}
+    >
+      <List>
+        {["Track", "Ship", "Contact Us"].map((text) => (
+          <ListItem
+            button
+            key={text}
+            onClick={() =>
+              handleNavigation(
+                text === "Track"
+                  ? "/track"
+                  : text === "Ship"
+                  ? "/ship"
+                  : "/contact"
+              )
+            }
+          >
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </Box>
   );
 
   return (
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static" color="info">
-          <Toolbar>
-            <IconButton
-                size="large"
-                edge="start"
-                color="inherit"
-                aria-label="menu"
-                sx={{ mr: 2 }}
-                onClick={handleDrawerToggle}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Delivery Co.
-            </Typography>
-            <Box sx={{ display: { xs: 'none', md: 'block' } }}>
-              <Button color="inherit" onClick={() => navigate('/track')}>Track</Button>
-              <Button color="inherit" onClick={() => navigate('/ship')}>Ship</Button>
-              <Button color="inherit" onClick={() => navigate('/contact')}>Contact Us</Button>
-            </Box>
-          </Toolbar>
-        </AppBar>
+    <Box sx={{ flexGrow: 1 }}>
+      <CssBaseline />
+      <AppBar position="static" color="info">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+
+          {(!isMobileSize) && (
+            <>
+              <Button color="inherit" href="/track">
+                Track
+              </Button>
+              <Button color="inherit" href="/ship">
+                Ship
+              </Button>
+              <Button color="inherit" href="/contact">
+                Contact Us
+              </Button>
+            </>
+          )}
+
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, textAlign: "right" }}
+          >
+            Delivery Co.
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <nav>
         <Drawer
-            anchor="left"
-            open={drawerOpen}
-            onClose={handleDrawerToggle}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true,
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: 250,
+            },
+          }}
         >
           {drawer}
         </Drawer>
-      </Box>
+      </nav>
+    </Box>
   );
-}
+};
 
 export default Navbar;
